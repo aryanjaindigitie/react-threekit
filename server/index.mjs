ee10dd419d4f11b6665d2a486995884db433e345
Amaan Saeed <amaansaeed0@gmail.com>
2021-04-07 11:53:58 -0400

    implements express server boilerplate

---
+++ server/index.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import morgan from 'morgan';
+
+//  Port setup
+const argv = process.argv.slice(2);
+const portIdx =
+  argv.indexOf('--port') !== -1 ? argv.indexOf('--port') : argv.indexOf('-p');
+const PORT = process.env.PORT || portIdx !== -1 ? argv[portIdx + 1] : 5000;
+
+const app = express();
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+app.use(morgan('tiny'));
+app.use(cors());
+
+//  React build
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log('listening on port: ', PORT));
