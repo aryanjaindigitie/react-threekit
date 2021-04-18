4e90ca22188d54cc782a1454a582221a2f1d0294
Amaan Saeed <amaansaeed0@gmail.com>
2021-04-18 12:55:41 -0400

    adds datatables routes

--- src/threekit/api/http/index.js
+++ src/threekit/api/http/index.js
@@ -3,7 +3,8 @@ import * as catalog from './catalog';
 import * as files from './files';
 import * as configurations from './configurations';
 import * as orgs from './orgs';
+import * as datatables from './datatables';
 
-const http = { orgs, products, catalog, files, configurations };
+const http = { orgs, products, catalog, files, configurations, datatables };
 
 export default http;
