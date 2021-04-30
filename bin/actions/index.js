992ddcc6bddf26fd588561fd0aac97910f93b975
Amaan Saeed <amaansaeed0@gmail.com>
2021-04-29 21:51:29 -0400

    removes server + cli

--- bin/actions/index.js
+++
@@ -1,8 +0,0 @@
-import * as catalog from './catalog';
-import * as configurations from './configurations';
-import * as datatables from './datatables';
-import * as demo from './demo';
-import init from './init';
-import * as languages from './languages';
-
-export default { catalog, configurations, datatables, demo, init, languages };
