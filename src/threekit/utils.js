1ed7e4a0632f580ee0e4dfb02695bdca49a13bc9
Amaan Saeed <amaansaeed0@gmail.com>
2021-07-21 22:02:35 -0400

    Adds camera helper utils

--- src/threekit/utils.js
+++ src/threekit/utils.js
@@ -234,3 +234,29 @@ export const selectionToConfiguration = (value, attributeType) => {
   }
   return updated;
 };
+
+export const getCameraPosition = (cameraApi) => ({
+  position: cameraApi.getPosition(),
+  quaternion: cameraApi.getQuaternion(),
+});
+
+export const setCameraPosition = (cameraApi, cameraPosition) => {
+  cameraApi.setPosition(cameraPosition.position);
+  cameraApi.setQuaternion(cameraPosition.quaternion);
+};
+
+export const dataURItoBlob = (dataURI) => {
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+    byteString = atob(dataURI.split(',')[1]);
+  else byteString = unescape(dataURI.split(',')[1]);
+
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], { type: mimeString });
+};
