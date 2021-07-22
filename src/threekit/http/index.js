789a1225c38245decb4528c8341a96af2b9e9569
Amaan Saeed <amaansaeed0@gmail.com>
2021-07-21 22:01:47 -0400

    adds files and order api and http modules

--- src/threekit/http/index.js
+++ src/threekit/http/index.js
@@ -1,7 +1,11 @@
+import * as orders from './orders';
 import * as products from './products';
 import * as configurations from './configurations';
+import * as files from './files';
 
 export default {
+  orders,
   products,
   configurations,
+  files,
 };
