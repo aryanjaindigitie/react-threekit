1d1bbf44a402a29f0000cb3f77c2c3d908afa0e1
Amaan Saeed <amaansaeed0@gmail.com>
2021-07-23 18:05:33 -0400

    updates config and packages for bundling server

--- config/paths.js
+++ config/paths.js
@@ -3,6 +3,9 @@ const path = require('path');
 const appRoot = path.join(__dirname, '..');
 const appPackageJson = path.join(appRoot, 'package.json');
 const appBuild = path.join(appRoot, 'build');
+const serverBuild = path.join(appRoot, 'server-build');
+const serverRoot = path.join(appRoot, 'server');
+const serverIndexJs = path.join(serverRoot, 'index.js');
 const appPublic = path.join(appRoot, 'public');
 const appIndexHtml = path.join(appPublic, 'index.html');
 const appSrc = path.join(__dirname, '..', 'src');
@@ -14,6 +17,9 @@ const nodeModules = path.join(appSrc, 'node_modules');
 module.exports = {
   appPackageJson,
   appBuild,
+  serverBuild,
+  serverRoot,
+  serverIndexJs,
   appPublic,
   appIndexHtml,
   appSrc,
