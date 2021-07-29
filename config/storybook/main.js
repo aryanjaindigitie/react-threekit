c389f9f6639a855d676e1091877c251107b79b2f
Amaan Saeed <amaansaeed0@gmail.com>
2021-07-29 12:18:54 -0400

    updates storybook config

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: [
     '../../src/**/*.stories.mdx',
     '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../threekit/**/*.stories.mdx',
+    '../../threekit/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
